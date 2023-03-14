dc4d78c86fbda04e20a2160967d1d3836dd8fbac
juhsanttos13 <juliasantosdasilva015@gmail.com>
2023-03-14 14:26:45 -0300

    AulaAlgoritmo

---
+++ algoritmo.js
@@ -0,0 +1,16 @@
+var numeroUm = 0;
+let numeroDois = 10;
+let numeroTres = 20;
+
+//diferença entre const e var
+//diferença entre let e var (diferença de escopo)
+
+console.log(numeroUm)
+ //para que serve o console.log e como usá-lo.
+ 
+ numeroUm = numeroDois;
+ console.log(numeroUm)
+ 
+ numeroUm = numeroTres;
+ console.log(numeroUm);
+ 
---
+++ algumacoisa.js
@@ -0,0 +1,8 @@
+if(true){
+    let a=10;
+    console.log(a);
+
+}
+
+//console.log(a) fora de escopo
+//diferença entre let e var(direfença de escopo)
